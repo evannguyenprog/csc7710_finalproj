be401c19370dad5763a0df357076ebd3966d492e
Evan Nguyen <48500302+evannguyenprog@users.noreply.github.com>
2022-12-11 17:36:23 -0500

    basic html outline

--- app.py
+++ app.py
@@ -9,34 +9,34 @@ mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
 mongo_db = mongo_client['evan_rentals']
 mongo_users_coll = mongo_db['users']
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates', static_folder='staticFiles')
 
 
 @app.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        user_profile = requests.get("http://127.0.0.1:5003/users/{}".format(session.get('user')))
-        user_profile = user_profile.json()
-        welcome_message = "Welcome " + user_profile["First_Name"] + "!"
-        return welcome_message
+    # if not session.get('logged_in'):
+    #     return render_template('login.html')
+    # else:
+        # user_profile = requests.get("http://127.0.0.1:5003/users/{}".format(session.get('user')))
+        # user_profile = user_profile.json()
+        # welcome_message = "Welcome!"
+        return render_template('home_page.html')
         
-@app.route('/login', methods=['POST'])
-def authenticate():
+# @app.route('/login', methods=['POST'])
+# def authenticate():
 
-    print(request)
-    query = {"Username": request.form['username'], "Password": request.form['password']}
+#     print(request)
+#     query = {"Username": request.form['username'], "Password": request.form['password']}
 
-    valid_login = mongo_users_coll.count_documents(query)
+#     valid_login = mongo_users_coll.count_documents(query)
 
-    if valid_login == 1:
-        session['logged_in'] = True
-        session['user'] = request.form['username']
-        #return userHomePage()
-    else:
-        flash('Invalid Login Attempt')
-    return home()
+#     if valid_login == 1:
+#         session['logged_in'] = True
+#         session['user'] = request.form['username']
+#         #return userHomePage()
+#     else:
+#         flash('Invalid Login Attempt')
+#     return home()
 
 
 if __name__ == '__main__':
