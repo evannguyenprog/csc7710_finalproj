16f47333b00506e6a997b4b9bd685266ee713c54
Evan Nguyen <48500302+evannguyenprog@users.noreply.github.com>
2022-12-12 03:57:20 -0500

    update and delete done

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from werkzeug.utils import redirect
 from bson.json_util import dumps, loads
 from bson import ObjectId
 import numpy as np
-from datetime import date
+from datetime import date, datetime
 import uuid
 
 
@@ -69,19 +69,49 @@ def createBookingQuery():
             'Date': str(today),
             'RideID': ObjectId(search_id),
             'TicketID': str(uuid.uuid4()),
-            'Price': cost}
+            'Cost': cost}
     collection_purchased.insert_one(query)
     return dumps(query)
    
-@app.route('/deleteBooking')
+@app.route('/deleteBooking', methods=['GET', 'POST'])
 def deleteBooking():
     return render_template('delete_booking.html')
 
-@app.route('/deleteBookingQuery')
+@app.route('/deleteBookingQuery', methods=['GET', 'POST'])
 def deleteBookingQuery():
-    #delete logic here
-    return render_template('create_booking.html')
+    ticket_to_delete = request.form['deleteid']
+    collection_purchased.delete_one({"TicketID":ticket_to_delete})
+    return ("Ticket with id: [" + ticket_to_delete + "] successfully deleted.")
 
+@app.route('/updateBooking', methods=['GET', 'POST'])
+def updateBooking():
+    return render_template('update_booking.html')
+
+@app.route('/updateBookingQuery', methods=['GET', 'POST'])
+def updateBookingQuery():
+
+    nDict = dict()
+    search_id = request.form['newRideID'] #to create search string with
+    print(search_id)
+    first_query = collection_openings.find_one({"_id":ObjectId(search_id)})
+    nDict = first_query
+    cost = nDict.get('Cost')
+    #newUUID = str(uuid.uuid4())
+    query = {'TicketID': request.form['updateid']}
+
+    updates = {"$set": {'RideID': ObjectId(request.form['newRideID']), 'Date': str(datetime.now()), 'Cost': cost, }}
+
+    collection_purchased.update_one(query, updates)
+
+    ticket_to_update = request.form['updateid']
+    return ("Ticket with id: [" + ticket_to_update + "] successfully updated.")
+
+
+@app.route('/viewBookings', methods=['GET'])
+def returnAllBookings():
+    #cursor = collection_openings.find({"_id":ObjectId('63965d890a9fd79931f89e9d')})
+    bookings = collection_purchased.find()
+    return dumps(bookings)
 
 # @app.route('/login', methods=['POST'])
 # def authenticate():
