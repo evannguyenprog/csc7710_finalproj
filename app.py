872b9dd57ef30e9a6521b844bf994f7000758096
Evan Nguyen <48500302+evannguyenprog@users.noreply.github.com>
2022-12-12 00:48:47 -0500

    create booking done

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import requests
 from werkzeug.utils import redirect
 from bson.json_util import dumps, loads
 from bson import ObjectId
+import numpy as np
+from datetime import date
+import uuid
+
+
+today = date.today()
 
 mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
 mongoDB = mongo_client['evan_rentals']
@@ -29,19 +35,6 @@ def returnOpenings():
     openings = collection_openings.find()
     return dumps(openings)
 
-@app.route('/createBooking')
-def returnRides():
-    newDict = dict()
-    x = 0
-    y = 0
-    data = collection_openings.find()
-    print("data sent to page")
-    for document in data:
-        newDict[x] = document
-        x+=1
-
-    return render_template('create_booking.html', data = data, y = y)
-
 @app.route('/bookingsPage')
 def returnBookingsPage():
     return render_template('bookings.html')
@@ -56,6 +49,39 @@ def returnBookings():
     query = {'TicketID': request.form['ticketid']}
     results = collection_purchased.find_one(query)
     return dumps(results)
+
+@app.route('/createBooking')
+def createBooking():
+    return render_template('create_booking.html')
+
+@app.route('/createBookingQuery', methods=['GET', 'POST'])
+def createBookingQuery():
+    #querydb for price and id
+    nDict = dict()
+    search_id = request.form['rideid'] #to create search string with
+    print(search_id)
+    first_query = collection_openings.find_one({"_id":ObjectId(search_id)})
+    nDict = first_query
+    cost = nDict.get('Cost')
+    # #cursor = collection_openings.find({"_id":ObjectId('63965d890a9fd79931f89e9d')})
+
+    query = {'Name': request.form['name'],
+            'Date': str(today),
+            'RideID': ObjectId(search_id),
+            'TicketID': str(uuid.uuid4()),
+            'Price': cost}
+    collection_purchased.insert_one(query)
+    return dumps(query)
+   
+@app.route('/deleteBooking')
+def deleteBooking():
+    return render_template('delete_booking.html')
+
+@app.route('/deleteBookingQuery')
+def deleteBookingQuery():
+    #delete logic here
+    return render_template('create_booking.html')
+
 
 # @app.route('/login', methods=['POST'])
 # def authenticate():
